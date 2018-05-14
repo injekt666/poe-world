95a3771f0fa39b6fc75811065e4e0c4c8a69a00b
Pascal Boutin <pboutin@mirego.com>
2018-05-13 22:59:36 -0400

    Initialize centered atlas

--- app/services/reframers/atlas-reframer.js
+++ app/services/reframers/atlas-reframer.js
@@ -2,6 +2,8 @@ import Service from '@ember/service';
 import {task, timeout} from 'ember-concurrency';
 
 // Constants
+const ATLAS_WIDTH = 4096;
+const ATLAS_HEIGHT = 2304;
 const SIDE_PANEL_WIDTH = 600;
 const MAX_ZOOM = 1;
 const MIN_ZOOM = 0.5;
@@ -15,7 +17,8 @@ export default Service.extend({
   isReframing: false,
 
   mapReframingTask: task(function *(map) {
-    const viewportCenterX = (window.innerWidth - SIDE_PANEL_WIDTH) / 2;
+    const viewportCenterX = window.innerWidth / 2;
+    const remainingViewportCenterX = (window.innerWidth - SIDE_PANEL_WIDTH) / 2;
     const viewportCenterY = window.innerHeight / 2;
     const {scale: zoom} = this._panzoomRef.getTransform();
 
@@ -33,7 +36,7 @@ export default Service.extend({
     };
 
     this._panzoomRef.moveBy(
-      viewportCenterX - currentOffset.x,
+      remainingViewportCenterX - currentOffset.x,
       viewportCenterY - currentOffset.y,
       true
     );
@@ -54,13 +57,33 @@ export default Service.extend({
     this.set('isReframing', false);
   }).restartable(),
 
+  initializeFrameTask: task(function *task(initialMap) {
+    const viewportCenterX = window.innerWidth / 2;
+    const viewportCenterY = window.innerHeight / 2;
+
+    this._panzoomRef.moveBy(
+      viewportCenterX - ATLAS_WIDTH / 2,
+      viewportCenterY - ATLAS_HEIGHT / 2,
+      false
+    );
+
+    this._panzoomRef.zoomAbs(
+      viewportCenterX,
+      viewportCenterY,
+      MIN_ZOOM
+    );
+
+    if (!initialMap) return;
+
+    yield timeout(ZOOM_DURATION)
+
+    yield this.mapReframingTask.perform(initialMap);
+  }).restartable(),
+
   initialize(panzoomRef) {
     this._panzoomRef = panzoomRef;
 
-    if (!this._mapToReframeOnInitialize) return;
-
-    this.reframeFor(this._mapToReframeOnInitialize);
-    this._mapToReframeOnInitialize = null;
+    this.initializeFrameTask.perform(this._mapToReframeOnInitialize);
   },
 
   reframeFor(map) {
