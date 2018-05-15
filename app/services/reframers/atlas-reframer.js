f455b11df63895b239a06e9fe37cad63fc46c1fc
Pascal Boutin <pboutin@mirego.com>
2018-05-14 20:33:09 -0400

    Reverse the panzoom map reframe order

--- app/services/reframers/atlas-reframer.js
+++ app/services/reframers/atlas-reframer.js
@@ -1,15 +1,17 @@
 import Service from '@ember/service';
 import {task, timeout} from 'ember-concurrency';
 
+/* eslint-disable no-magic-numbers */
 // Constants
 const ATLAS_WIDTH = 4096;
 const ATLAS_HEIGHT = 2304;
 const SIDE_PANEL_WIDTH = 600;
 const MAX_ZOOM = 1;
-const MAX_ZOOM_THRESHOLD = 0.99;
+const MAX_ZOOM_THRESHOLD = 0.99 * MAX_ZOOM;
 const MIN_ZOOM = 0.5;
-const ZOOM_DURATION = 400;
+const ZOOM_DURATION = 500;
 const MOVE_DURATION = 500;
+/* eslint-enable no-magic-numbers */
 
 export default Service.extend({
   _panzoomRef: null,
@@ -18,33 +20,31 @@ export default Service.extend({
   isReframing: false,
 
   mapReframingTask: task(function *(map) {
-    const viewportCenterX = window.innerWidth / 2;
-    const remainingViewportCenterX = (window.innerWidth - SIDE_PANEL_WIDTH) / 2;
-    const viewportCenterY = window.innerHeight / 2;
-    const {scale: zoom} = this._panzoomRef.getTransform();
-
     this.set('isReframing', true);
+
+    const viewportCenterX = (window.innerWidth - SIDE_PANEL_WIDTH) / 2;
+    const viewportCenterY = window.innerHeight / 2;
+
+    const {x: panLeft, y: panTop, scale: zoom} = this._panzoomRef.getTransform();
+
+    const currentOffset = {
+      x: (map.offsetLeft * zoom) - (panLeft * -1),
+      y: (map.offsetTop * zoom) - (panTop * -1)
+    };
+
+    this._panzoomRef.moveBy(
+      (viewportCenterX - currentOffset.x),
+      (viewportCenterY - currentOffset.y),
+      true
+    );
+
+    yield timeout(MOVE_DURATION);
 
     if (zoom < MAX_ZOOM_THRESHOLD) {
       this._panzoomRef.smoothZoom(viewportCenterX, viewportCenterY, MAX_ZOOM / zoom);
 
       yield timeout(ZOOM_DURATION);
     }
-
-    const {x: panLeft, y: panTop} = this._panzoomRef.getTransform();
-
-    const currentOffset = {
-      x: map.offsetLeft - (panLeft * -1),
-      y: map.offsetTop - (panTop * -1)
-    };
-
-    this._panzoomRef.moveBy(
-      remainingViewportCenterX - currentOffset.x,
-      viewportCenterY - currentOffset.y,
-      true
-    );
-
-    yield timeout(MOVE_DURATION);
 
     this.set('isReframing', false);
   }).restartable(),
@@ -78,7 +78,7 @@ export default Service.extend({
 
     if (!initialMap) return;
 
-    yield timeout(ZOOM_DURATION)
+    yield timeout(ZOOM_DURATION);
 
     yield this.mapReframingTask.perform(initialMap);
   }).restartable(),
