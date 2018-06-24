50e5beb414384a170d0cd0c7b49f60978ae16a6d
Pascal Boutin <pboutin@mirego.com>
2018-06-24 12:33:42 -0400

    Refactor the application-mode to global-state

--- app/pods/components/pow-external-link/component.js
+++ app/pods/components/pow-external-link/component.js
@@ -2,7 +2,7 @@ import Component from '@ember/component';
 import {inject as service} from '@ember/service';
 
 export default Component.extend({
-  applicationMode: service('application-mode'),
+  globalState: service('global-state'),
   electronUrlOpener: service('electron/url-opener'),
 
   tagName: 'a',
@@ -11,9 +11,9 @@ export default Component.extend({
 
   click(event) {
     event.preventDefault();
-    const {url, applicationMode, electronUrlOpener} = this.getProperties('url', 'applicationMode', 'electronUrlOpener');
+    const {url, globalState, electronUrlOpener} = this.getProperties('url', 'globalState', 'electronUrlOpener');
 
-    if (applicationMode.isWeb()) return window.open(url, '_blank').focus();
+    if (globalState.isWeb) return window.open(url, '_blank').focus();
 
     electronUrlOpener.open(url);
   }
