05284f35d9a6d53dec993d34ed83a16cc3745948
Pascal Boutin <pboutin@mirego.com>
2018-06-26 20:55:24 -0400

    Introduce proper config for windows building :package:

--- app/services/electron/.eslintrc.js
+++ app/services/electron/.eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   globals: {
-    requireNode: true
+    require: true,
+    module: true
   }
 }
--- ember-electron/electron-forge-config.js
+++ ember-electron/electron-forge-config.js
@@ -1,30 +1,41 @@
+/* eslint-disable */
+const path = require('path');
+
 module.exports = {
-  "make_targets": {
-    "win32": [
-      "squirrel"
+  make_targets: {
+    win32: ['squirrel']
+  },
+  electronPackagerConfig: {
+    appCopyright: `Copyright (c) 2016-${(new Date().getFullYear())} PoeWorld`,
+    name: 'PoeWorld',
+    versionString: {
+      CompanyName: 'PoeWorld',
+      FileDescription: 'for Desktops',
+      ProductName: 'PoeWorld',
+      InternalName: 'PoeWorld'
+    },
+    protocols: [
+      {
+        name: 'PoeWorld Desktop',
+        schemes: ['poe-world']
+      }
     ],
-    "darwin": [
-      "zip"
-    ],
-    "linux": [
-      "deb",
-      "rpm"
-    ]
+    protocol: ['poe-world'],
+    protocolName: 'PoeWorld Desktop',
+    overwrite: true,
+    icon: 'ember-electron/assets/icons/poe-world'
   },
-  "electronPackagerConfig": {
-    "packageManager": "npm"
-  },
-  "electronWinstallerConfig": {
-    "name": "pow"
-  },
-  "electronInstallerDebian": {},
-  "electronInstallerRedhat": {},
-  "github_repository": {
-    "owner": "pboutin",
-    "name": "pow-world"
-  },
-  "windowsStoreConfig": {
-    "packageName": "",
-    "name": "pow"
+  electronWinstallerConfig: {
+    name: 'PoeWorld',
+    icon: 'ember-electron/assets/icons/poe-world',
+    noMsi: true,
+    authors: 'PoeWorld',
+    exe: 'PoeWorld.exe',
+    iconUrl: 'https://raw.githubusercontent.com/poe-world/poe-world/master/ember-electron/assets/icons/poe-world.ico',
+    setupIcon: path.join(__dirname, 'assets/icons/poe-world.ico'),
+    title: 'PoeWorld',
+    noMsi: true,
+    loadingGif: path.join(__dirname, 'assets/win32/installer.gif')
   }
 };
+/* eslint-enable */
