0d953177135be6afb89b609497b50de6e2865fcb
Pascal Boutin <pboutin@mirego.com>
2018-08-10 15:02:25 -0400

    Refactor the base of modal styling

--- app/pods/components/pow-modal/component.js
+++ app/pods/components/pow-modal/component.js
@@ -1,7 +1,12 @@
 import Component from '@ember/component';
+import {equal} from '@ember/object/computed';
 
 export default Component.extend({
   title: '',
   isOpened: false,
-  onClose: () => {}
+  onClose: () => {},
+  size: null,
+
+  isLarge: equal('size', 'large'),
+  isSmall: equal('size', 'small')
 });
