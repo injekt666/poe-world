c9db0cd5147e25bc9479e0e098ddb102590664ac
Pascal Boutin <pboutin@mirego.com>
2018-08-10 16:40:07 -0400

    Replace ember-modal-dialog for vanilla bootstrap modal

--- app/pods/components/pow-modal/component.js
+++ app/pods/components/pow-modal/component.js
@@ -1,5 +1,6 @@
 import Component from '@ember/component';
 import {equal} from '@ember/object/computed';
+import {observer} from '@ember/object';
 
 export default Component.extend({
   title: '',
@@ -8,5 +9,23 @@ export default Component.extend({
   size: null,
 
   isLarge: equal('size', 'large'),
-  isSmall: equal('size', 'small')
+  isSmall: equal('size', 'small'),
+
+  openedStateObserver: observer('isOpened', function() {
+    if (this.isOpened) return this.$('[data-toggle="modal"]').modal('show');
+    return this.$('[data-toggle="modal"]').modal('hide');
+  }),
+
+  didInsertElement() {
+    this.$('[data-toggle="modal"]')
+      .modal({
+        backdrop: true,
+        keyboard: true,
+        focus: false,
+        show: this.isOpened
+      })
+      .on('hidden.bs.modal', function() {
+        if (this.isOpened) this.onClose();
+      });
+  }
 });
