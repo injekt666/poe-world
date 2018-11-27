b1491747c153a8bce5f9369e17cb645d341677c3
Pascal Boutin <pboutin@mirego.com>
2018-11-26 23:05:06 -0500

    Fix typo in the desktop relase

--- scripts/release-desktop.sh
+++ scripts/release-desktop.sh
@@ -12,7 +12,7 @@ echo "Releasing to GitHub..."
 version=`git describe --tags --abbrev=0`
 escaped_version=${version//./_}
 changelog_file=./changelogs/$escaped_version.md
-attachment=./electron-out/make/squirrel.windows/x64/PowWorld-$version\ Setup.exe
+attachment=./electron-out/make/squirrel.windows/x64/PoeWorld-$version\ Setup.exe
 hub release create --file=$changelog_file --attach=$attachment --browse $version
 
 echo "Switching back to the original branch..."
