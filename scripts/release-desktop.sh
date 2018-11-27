3f6027f6372b06f4f90d1058d558ae2c8cd4a043
Pascal Boutin <contact@pboutin.net>
2018-11-26 23:19:48 -0500

    Fix the hub release call

--- scripts/release-desktop.sh
+++ scripts/release-desktop.sh
@@ -11,9 +11,7 @@ npm run build-electron
 echo "Releasing to GitHub..."
 version=`git describe --tags --abbrev=0`
 escaped_version=${version//./_}
-changelog_file=./changelogs/$escaped_version.md
-attachment=./electron-out/make/squirrel.windows/x64/PoeWorld-$version\ Setup.exe
-hub release create --file=$changelog_file --attach=$attachment --browse $version
+hub release create --file="./changelogs/$escaped_version.md" --attach="./electron-out/make/squirrel.windows/x64/PoeWorld-$version Setup.exe" --browse $version
 
 echo "Switching back to the original branch..."
 git checkout $current_branch
