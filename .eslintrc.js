6ebcba520046cc067fdcf306d15fe5c20f8af7f2
Pascal Boutin <pboutin@mirego.com>
2018-11-27 21:32:52 -0500

    Plug deps and setup configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,16 @@ module.exports = {
     es6: true
   },
   rules: {
+    'ember/closure-actions': 2,
+    'ember/named-functions-in-promises': 2,
+    'ember/new-module-imports': 2,
+    'ember/no-global-jquery': 2,
+    'ember/no-on-calls-in-components': 2,
+    'ember/no-duplicate-dependent-keys': 2,
+    'ember/no-side-effects': 2,
+    'ember/require-super-in-init': 2,
+    'ember/avoid-leaking-state-in-ember-objects': 2,
+    'ember/use-brace-expansion': 2
   },
   overrides: [
     // node files
